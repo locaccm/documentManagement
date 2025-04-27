6885d3c67a939495ba66305a7c7bdb2baaf731b0
ClemZi02 <clement.foulon19@gmail.com>
2025-04-27 20:36:13 +0200

    fix: [AIC-99] - modify extension for jest config file

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
