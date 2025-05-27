59a3cb10bbcb85248221399b838b7cabc05ca9f6
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2025-05-27 19:13:50 +0200

    Rename Cloud Run service to use underscores for consistency

--- terraform/cloudRun.tf
+++ terraform/cloudRun.tf
@@ -2,9 +2,9 @@ module "cloud_run_document_management" {
   source                = "./modules/cloud_run"
   project_id            = "intricate-pad-455413-f7"
   region                = "europe-west1"
-  service_name          = "documentmanagement-service"
+  service_name          = "document_management"
   repository_id         = "locaccm-repo-docker"
-  service_account_email = module.service_account_documentManagmenet.email
+  service_account_email = module.service_account_document_management.email
   vpc_connector         = module.vpc_connector.id
   public                = false
 
@@ -17,7 +17,7 @@ module "cloud_run_auth_invokers" {
   depends_on = [module.cloud_run_document_management]
   source        = "./modules/cloud_run_invoker"
   region        = "europe-west1"
-  service_name  = "documentmanagement-service"
+  service_name  = "document_management"
   invokers = {
     frontend            = "frontend-service@intricate-pad-455413-f7.iam.gserviceaccount.com"
     wealthmanagement    = "wealthmanagement-service@intricate-pad-455413-f7.iam.gserviceaccount.com"
